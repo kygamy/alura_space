13419f158e503ad188ee4f40585728f6322d0e19
Victor Gabriel <vgabrielcunhabezerra@gmail.com>
2024-01-17 09:03:49 -0300

    terceiro commit

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 
 
@@ -60,3 +61,22 @@ class CadastroForms(forms.Form):
             attrs={"class": "form-control", "placeholder": "Digite sua senha novamente"}
         ),
     )
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get("nome_cadastro")
+        if nome:
+            nome = nome.strip()
+            if " " in nome:
+                raise forms.ValidationError("Não pode haver espaços nesse campo ↑")
+            else:
+                return nome
+
+    def clean_senha_2(self):
+        senha_1 = self.cleaned_data.get("senha_1")
+        senha_2 = self.cleaned_data.get("senha_2")
+
+        if senha_1 and senha_2:
+            if senha_1 != senha_2:
+                raise forms.ValidationError("Senhas não estão iguais")
+            else:
+                return senha_2
